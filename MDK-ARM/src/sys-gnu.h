5ebcfe64f816a9f57b240fec6517a5c74b4ce23d
x893 <x893.00@gmail.com>
2014-12-23 22:35:18 +0300

    Initial commit

--- MDK-ARM/src/sys-gnu.h
+++ MDK-ARM/src/sys-gnu.h
@@ -7,17 +7,18 @@
 
 #include <stdbool.h>
 #include <sys/types.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#define	ENOEXEC	8	/* Exec format error */
-#define	ENOENT	2	/* No such file or directory */
-#define	EIO		5	/* I/O error */
-#define	E2BIG	7	/* Arg list too long */
-#define	EEXIST	17	/* File exists */
+#define	ENOEXEC		8	/* Exec format error */
+#define	ENOENT		2	/* No such file or directory */
+#define	EIO			5	/* I/O error */
+#define	E2BIG		7	/* Arg list too long */
+#define	EEXIST		17	/* File exists */
 
-#define ENETDOWN 115		/* Network interface is not configured */
+#define ENETDOWN	115	/* Network interface is not configured */
 
 #define M_PI		3.14159265358979323846
 
